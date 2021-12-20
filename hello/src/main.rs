183acbfb5a008f5a20fe32702ec71822c672ebd2
&shy <willjarvin@gmail.com>
2021-12-20 14:56:28 +0800

    update gitignore etc

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,4 +1,7 @@
-fn gcd(mut n: u64, mut m: u64) -> u64 {
+use std::io::Write;
+use std::str::FromStr;
+
+fn gcd(mut n: u64: mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
         if m < n {
@@ -8,21 +11,34 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
         }
         m = m % n;
     }
-    // Rust 有return语句，但gcd函数中没有。如果函数体中最后一行代码是一个表达式，且表达式末尾没有分号，那这个表达式的值就是函数的返回值
     n
 }
 
 #[test]
 fn test_gcd() {
-         assert_eq!(gcd(14, 15), 1);
-         assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
-                        3 * 7 * 11 * 13 * 19),
-                    3 * 11);
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 1);
 }
-
 
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+                        .expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
+
 
 
